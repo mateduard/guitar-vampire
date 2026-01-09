22f3e87665ed80955b315c95bb3aca531ef6dcd5
mateduard <edy_angel_2008@yahoo.com>
2026-01-09 13:53:49 +0200

    finished jenkins. last commit

--- deployment/terraform/aks.tf
+++ deployment/terraform/aks.tf
@@ -32,12 +32,11 @@ resource "azurerm_kubernetes_cluster" "aks" {
   # Disable private cluster
   private_cluster_enabled = false
 
-  # No fleet manager, AZ, or other advanced options specified
   # Node pool configuration (default node pool)
   default_node_pool {
     name       = "nodepool1"
     node_count = 1
-    vm_size    = "Standard_D2ls_v5" # default size, can be customized
+    vm_size    = "Standard_D2ls_v5"
   }
 
   # Additional features like Azure Policy, Prometheus, Grafana, etc., are disabled by default
