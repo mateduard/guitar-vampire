964f05feab00f4a86e551c7f2b50042041182295
mateduard <edy_angel_2008@yahoo.com>
2026-02-04 00:10:47 +0200

    output paths metrics at /metrics endpont

--- server/routes/guitars.js
+++ server/routes/guitars.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const Guitar = require('../models/Guitar');
+const client = require('prom-client');
+
+const guitarAccessCounter = new client.Counter({
+  name: 'guitar_route_access_total',
+  help: 'Number of times the /api/guitars has been accessed',
+});
 
 router.get('/', async (req, res) => {
     try{
+        console.log("Someone hit the guitars API page (/api/guitars)");
+        guitarAccessCounter.inc();
         const guitars = await Guitar.find();
         res.json({success: true, response: guitars});
     }
@@ -13,4 +21,7 @@ router.get('/', async (req, res) => {
     }
 })
 
-module.exports = router;
+module.exports = {
+    router,
+    guitarAccessCounter,
+};
